8ce2a84476173a52bd811e9ec6017524da998140
Maxime PETIT <contactmaximepetit@gmail.com>
2021-04-26 22:36:01 +0200

    fix bug

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -19,7 +19,7 @@ new ScrollMagic.Scene({
 
 
 .setTween(introAnim)
-.addIndicators()
+// .addIndicators()
 .addTo(controller);
 
 
@@ -37,53 +37,124 @@ new ScrollMagic.Scene({
 
 
 .setTween(introtextAnim)
-.addIndicators()
+// .addIndicators()
 .addTo(controller);
 
 
 
 // Phone anim
 
-var phoneAnim = new TimelineMax()
+var controller = new ScrollMagic.Controller();
 
+var phoneFixed = new TimelineMax()
 
+// .to(".phone", 0.5, {opacity: 0}, 5)
+// .to(".livraison-app", 0.5, {opacity: 1}, 5.2)
 
 new ScrollMagic.Scene({
-    triggerElement: ".phone",
-    triggerHook:0.1,
-    duration: '1100'
+    triggerElement: ".how-it-works",
+    triggerHook: 0.1,
+    duration: '1303'
 })
 
 .setPin(".phone")
-.addIndicators( )
+.setTween(phoneFixed)
+.addIndicators()
 .addTo(controller);
 
+var phoneVisibility = new TimelineMax()
 
-// Change phone
-
-
-
-var changePhone2 = new TimelineMax()
-.to(".phone-img",{attr:{src: "assets/images/appvisu-commandes.png"}}, 0.22)
+.to(".livraison-app", 0.0001, {visibility: "visible"}, 0)
 
 new ScrollMagic.Scene({
-    triggerElement: ".commande",
-    triggerHook:0.3,
+    triggerElement: ".phone-2",
+    triggerHook: 0
 })
 
-.setTween(changePhone2)
-.addIndicators( )
+.setTween(phoneVisibility)
+// .addIndicators()
 .addTo(controller);
 
-var changePhone3 = new TimelineMax()
+var phoneBase = new TimelineMax()
 
-.to(".phone-img",{attr:{src: "assets/images/appvisu-livraison.png"}})
+.to(".phone", {attr:{src: "assets/images/appvisu-inscription.png"}}, 0)
 
 new ScrollMagic.Scene({
-    triggerElement: ".livraison",
-    triggerHook:0.5,
+    triggerElement: ".step1",
+    triggerHook: 0.3
 })
 
-.setTween(changePhone3)
-.addIndicators( )
+.setTween(phoneBase)
+.addIndicators()
 .addTo(controller);
+
+var phoneChange1 = new TimelineMax()
+
+.to(".phone", {attr:{src: "assets/images/appvisu-commandes.png"}}, 0)
+
+new ScrollMagic.Scene({
+    triggerElement: ".step2",
+    triggerHook: 0.3
+})
+
+.setTween(phoneChange1)
+.addIndicators()
+.addTo(controller);
+
+var phoneChange2 = new TimelineMax()
+
+.to(".phone", {attr:{src: "assets/images/appvisu-livraison.png"}}, 0)
+
+
+new ScrollMagic.Scene({
+    triggerElement: ".step3",
+    triggerHook: 0.3
+})
+
+.setTween(phoneChange2)
+.addIndicators()
+.addTo(controller);
+
+// var phoneAnim = new TimelineMax()
+
+
+
+// new ScrollMagic.Scene({
+//     triggerElement: ".phone",
+//     triggerHook:0.1,
+//     duration: '2000%'
+// })
+
+// .setPin(".phone")
+// .addIndicators( )
+// .addTo(controller);
+
+
+// // Change phone
+
+
+
+// var changePhone2 = new TimelineMax()
+// .to(".phone-img",{attr:{src: "assets/images/appvisu-commandes.png"}}, 0.22)
+
+// new ScrollMagic.Scene({
+//     triggerElement: ".commande",
+//     triggerHook:0.3,
+// })
+
+// .setTween(changePhone2)
+// .addIndicators(  )
+// .addTo(controller);
+
+// var changePhone3 = new TimelineMax()
+
+// .to(".phone-img",{attr:{src: "assets/images/appvisu-livraison.png"}})
+
+// new ScrollMagic.Scene({
+//     triggerElement: ".livraison",
+//     triggerHook:0.5,
+// })
+
+// .setTween(changePhone3)
+// .addIndicators( )
+// .addTo(controller);
