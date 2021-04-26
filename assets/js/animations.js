07c18fae3e0fe15c101c36314e23302c2bbe9c5f
Maxime PETIT <contactmaximepetit@gmail.com>
2021-04-26 23:20:28 +0200

    animation

---
+++ assets/js/animations.js
@@ -0,0 +1,45 @@
+const headerTop = document.querySelectorAll('.header-top');
+const headerContain = document.querySelector('.header-contain')
+const scroll = document.querySelector('.scroll')
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    // .from(logo, 0.2, {transform: "scale(0)", ease: "power2.out"}, 0.3)
+    .from(headerTop, 1.5, {opacity: 0, ease: "power2.out"}, 0.2)
+    .from(headerContain, 1.5, {translateY: '10px', opacity: 0, ease: "power2.out"}, 0.4, '-=1')
+    .from(scroll, 1, {opacity: 0, ease: "power2.out"}, 5, '-=2')
+
+    
+    
+
+    TL.play();
+})
+
+  
+// const titreSpans = document.querySelectorAll('h1 span');
+// const btns = document.querySelectorAll('.btn-first');
+// const logo = document.querySelector('.logo');
+// const medias = document.querySelectorAll('.bulle');
+// const l1 = document.querySelector('.l1');
+// const l2 = document.querySelector('.l2');
+
+// window.addEventListener('load', () => {
+
+//     const TL = gsap.timeline({paused: true});
+
+//     TL
+//     .staggerFrom(titreSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+//     .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+//     .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+//     .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+//     .from(logo, 0.4, {transform: "scale(0)", ease: "power2.out"}, '-=2')
+//     .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.3, '-=1');
+
+    
+    
+
+//     TL.play();
+// })
