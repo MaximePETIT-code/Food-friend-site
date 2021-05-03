edcea26868a7847f16c71ed8256e4cabd808885f
Maxime PETIT <contactmaximepetit@gmail.com>
2021-05-03 15:32:19 +0200

    offer

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+function user() {
+    document.getElementById("user").style.display = "block";
+    document.getElementById("user-link").style.color = "#1A1A1A";
+    document.getElementById("restorer").style.display = "none";
+    document.getElementById("restorer-link").style.color = "#107775";
+    document.getElementById("delivery").style.display = "none";
+    document.getElementById("delivery-link").style.color = "#107775";
+}
+
+function restorer() {
+    document.getElementById("restorer").style.display = "block";
+    document.getElementById("restorer-link").style.color = "#1A1A1A";
+    document.getElementById("user").style.display = "none";
+    document.getElementById("user-link").style.color = "#107775";
+    document.getElementById("delivery").style.display = "none";
+    document.getElementById("delivery-link").style.color = "#107775";
+}
+
+function delivery() {
+    document.getElementById("delivery").style.display = "block";
+    document.getElementById("delivery-link").style.color = "#1A1A1A";
+    document.getElementById("user").style.display = "none";
+    document.getElementById("user-link").style.color = "#107775";
+    document.getElementById("restorer").style.display = "none";
+    document.getElementById("restorer-link").style.color = "#107775";
+}
