a42449eef8ebffb6074051880cb20041d3a2edf2
Maxime PETIT <contactmaximepetit@gmail.com>
2021-05-09 13:24:20 +0200

    texte

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,124 @@
+const headerTop = document.querySelectorAll('.header-top');
+const headerContain = document.querySelector('.header-contain');
+const scroll = document.querySelector('.scroll');
+const back = document.querySelector('.back');
+const contact = document.querySelector('.contact-content');
+
+
+if (window.matchMedia("(min-width: 1000px)").matches) {
+    window.addEventListener('load', () => {
+
+        const TL = gsap.timeline({paused: true});
+    
+        TL
+        .to(headerTop, 1, {opacity: 1, ease: "power2.out"}, 0)
+        .to(headerContain, 1.5, {translateY: '0px', opacity: 1, ease: "power2.out"}, 0, '-=1')
+        .to(scroll, 1, {opacity: 1, ease: "power2.out"}, 0.2, '-=2')
+        
+    
+        
+        
+    
+        TL.play();
+    })
+}
+
+if (window.matchMedia("(min-width: 1227px)").matches) {
+    window.onresize = function(event)
+    {
+        document.location.reload(true);
+    }
+
+    var controller = new ScrollMagic.Controller();
+
+
+    var scroller = new TimelineMax()
+
+    .to(".scroll", {opacity: 0}, 0)
+
+    new ScrollMagic.Scene({
+        triggerElement: ".intro",
+        triggerHook: 1
+    })
+
+    .setTween(scroller)
+    .addTo(controller);
+
+    
+    function splitScroll() {
+        const controller = new ScrollMagic.Controller();
+        new ScrollMagic.Scene({
+            duration: "1000px",
+            triggerElement: ".app_visu",
+            triggerHook: 0
+            })
+
+        .setPin(".app_visu")
+        .addTo(controller);
+    }
+    splitScroll();
+
+
+    var phoneChange1 = new TimelineMax()
+
+    .to(".phone", {attr:{src: "assets/images/appvisu-commandes.png"}}, 0)
+
+    new ScrollMagic.Scene({
+        triggerElement: ".step2",
+        triggerHook: 0.3
+    })
+
+    .setTween(phoneChange1)
+    .addTo(controller);
+
+    var phoneChange2 = new TimelineMax()
+
+    .to(".phone", {attr:{src: "assets/images/appvisu-livraison.png"}}, 0)
+
+    new ScrollMagic.Scene({
+        triggerElement: ".step3",
+        triggerHook: 0.3
+    })
+
+    .setTween(phoneChange2)
+    .addTo(controller);
+
+}
+
+var button = document.querySelectorAll('.button-contact');
+
+$(document).on('mousemove touch', function(e){
+  magnetize('.button-contact', e);
+});
+
+function magnetize(el, e){
+  var mX = e.pageX,
+      mY = e.pageY;
+  const item = $(el);
+  
+  const customDist = item.data('dist') * 20 || 120;
+  const centerX = item.offset().left + (item.width()/2);
+  const centerY = item.offset().top + (item.height()/2);
+  
+  var deltaX = Math.floor((centerX - mX)) * -0.45;
+  var deltaY = Math.floor((centerY - mY)) * -0.45;
+  
+  var distance = calculateDistance(item, mX, mY);
+    
+  if(distance < customDist){
+    TweenMax.to(item, 0.5, {y: deltaY, x: deltaX, scale:1.1});
+    item.addClass('magnet');
+  }
+  else {
+    TweenMax.to(item, 0.6, {y: 0, x: 0, scale:1});
+    item.removeClass('magnet');
+  }
+}
+
+function calculateDistance(elem, mouseX, mouseY) {
+  return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/1.4)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2.2)));
+}
+
 function user() {
     document.getElementById("user").style.display = "block";
     document.getElementById("user-link").style.color = "#1A1A1A";
